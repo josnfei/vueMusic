3c7982f6bf2889172577124d0c4188dd93989327
josnfei <josnfei@qq.com>
2018-01-31 23:30:47 +0800

    add vue init 和项目基础样式字体,及文件夹结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   hello world
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus">
+  /* stylus 的引入,不用url  less 可用也可以不用   */
+@import "./common/stylus/variable.styl";
+#app
+  color:red
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    131223
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+/* @import url('../common/stylus/variable.less'); */
+@import '../common/stylus/variable.less';
+
+.hello{
+  color:aqua;
+}
+
+</style>
