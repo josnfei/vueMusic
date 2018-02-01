c115e2070cdcf85bbd70c03edb3531f985608b77
josnfei <josnfei@qq.com>
2018-02-01 22:52:25 +0800

    add 3-2路由配置+顶导组件开发

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import 'babel-polyfill'  //解决es6的promise问题补丁
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'  //解决移动端300ms延迟
+
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Recomend from 'components/recommend/recommend'  //推荐
+import Singer from 'components/singer/singer'  //歌手
+import Rank from 'components/rank/rank'  //排行
+import Search from 'components/search/search' //搜索
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/recommend'  //默认路由
+    },
+    {
+      path: '/recommend',
+      name: 'Recomend',
+      component: Recomend
+    },
+    {
+      path:'/singer',
+      component: Singer
+    },
+    {
+      path:'/rank',
+      component: Rank
+    },
+    {
+      path:'/search',
+      component: Search
     }
   ]
 })
