b4d3f5f46a568c69cbf5c4e4cc491ad96a088472
josnfei <josnfei@qq.com>
2018-04-11 01:43:59 +0800

    测试本地一个仓库，对应码云和github仓库

--- src/common/js/singer.js
+++ src/common/js/singer.js
@@ -1,5 +1,5 @@
 // css 类传参
-// 在类的参数传递中我们用constructor() 进行传参。 传递参数后可以直接使用this.xxx进行调用。
+// 在类的参数传递中我们用constructor() 进行传参。 传递参数后可以直接使用this.xxx进行调用.。
 export default class Singer {
     constructor({ id, name }) {
         this.id = id
@@ -7,3 +7,4 @@ export default class Singer {
         this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`
     }
 }
+// 一下测试
